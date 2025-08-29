cfa729ad3ea25dcaf49b31840b21fa77ab009e5f
svenviktorjonsson <svenviktorjonsson@gmail.com>
2025-08-29 19:41:33 +0200

    added more colormaps

--- named_colormaps.js
+++ named_colormaps.js
@@ -19,7 +19,9 @@ export default {
     "viridis": {
         "points": [
             { "pos": 0.0, "color": [68, 1, 84], "order": 1 },
+            { "pos": 0.25, "color": [59, 82, 139], "order": 1 },
             { "pos": 0.5, "color": [33, 145, 140], "order": 1 },
+            { "pos": 0.75, "color": [94, 201, 98], "order": 1 },
             { "pos": 1.0, "color": [253, 231, 37], "order": 1 }
         ]
     },
@@ -30,6 +32,75 @@ export default {
             { "pos": 0.5, "color": [203, 70, 121], "order": 1 },
             { "pos": 0.75, "color": [248, 149, 64], "order": 1 },
             { "pos": 1.0, "color": [240, 249, 33], "order": 1 }
+        ]
+    },
+    "inferno": {
+        "points": [
+            { "pos": 0.0, "color": [0, 0, 4], "order": 1 },
+            { "pos": 0.25, "color": [87, 15, 109], "order": 1 },
+            { "pos": 0.5, "color": [187, 55, 84], "order": 1 },
+            { "pos": 0.75, "color": [249, 142, 8], "order": 1 },
+            { "pos": 1.0, "color": [252, 255, 164], "order": 1 }
+        ]
+    },
+    "magma": {
+        "points": [
+            { "pos": 0.0, "color": [0, 0, 4], "order": 1 },
+            { "pos": 0.25, "color": [87, 24, 113], "order": 1 },
+            { "pos": 0.5, "color": [180, 54, 122], "order": 1 },
+            { "pos": 0.75, "color": [251, 136, 97], "order": 1 },
+            { "pos": 1.0, "color": [252, 253, 191], "order": 1 }
+        ]
+    },
+    "cividis": {
+        "points": [
+            { "pos": 0.0, "color": [0, 32, 77], "order": 1 },
+            { "pos": 0.25, "color": [31, 70, 84], "order": 1 },
+            { "pos": 0.5, "color": [86, 109, 90], "order": 1 },
+            { "pos": 0.75, "color": [159, 148, 84], "order": 1 },
+            { "pos": 1.0, "color": [255, 233, 69], "order": 1 }
+        ]
+    },
+    "coolwarm": {
+        "points": [
+            { "pos": 0.0, "color": [59, 76, 192], "order": 1 },
+            { "pos": 0.25, "color": [144, 178, 254], "order": 1 },
+            { "pos": 0.5, "color": [220, 220, 220], "order": 1 },
+            { "pos": 0.75, "color": [245, 156, 125], "order": 1 },
+            { "pos": 1.0, "color": [180, 4, 38], "order": 1 }
+        ]
+    },
+    "seismic": {
+        "points": [
+            { "pos": 0.0, "color": [0, 0, 75], "order": 1 },
+            { "pos": 0.25, "color": [0, 150, 255], "order": 1 },
+            { "pos": 0.5, "color": [255, 255, 255], "order": 1 },
+            { "pos": 0.75, "color": [255, 100, 0], "order": 1 },
+            { "pos": 1.0, "color": [100, 0, 0], "order": 1 }
+        ]
+    },
+    "spring": {
+        "points": [
+            { "pos": 0.0, "color": [255, 0, 255], "order": 1 },
+            { "pos": 1.0, "color": [255, 255, 0], "order": 1 }
+        ]
+    },
+    "summer": {
+        "points": [
+            { "pos": 0.0, "color": [0, 128, 102], "order": 1 },
+            { "pos": 1.0, "color": [255, 255, 102], "order": 1 }
+        ]
+    },
+    "autumn": {
+        "points": [
+            { "pos": 0.0, "color": [255, 0, 0], "order": 1 },
+            { "pos": 1.0, "color": [255, 255, 0], "order": 1 }
+        ]
+    },
+    "winter": {
+        "points": [
+            { "pos": 0.0, "color": [0, 0, 255], "order": 1 },
+            { "pos": 1.0, "color": [0, 255, 128], "order": 1 }
         ]
     },
     "grays": {
@@ -43,6 +114,20 @@ export default {
             { "pos": 0.0, "color": [0, 0, 0], "order": 1 },
             { "pos": 0.33, "color": [255, 0, 0], "order": 1 },
             { "pos": 0.67, "color": [255, 255, 0], "order": 1 },
+            { "pos": 1.0, "color": [255, 255, 255], "order": 1 }
+        ]
+    },
+    "copper": {
+        "points": [
+            { "pos": 0.0, "color": [0, 0, 0], "order": 1 },
+            { "pos": 1.0, "color": [255, 197, 123], "order": 1 }
+        ]
+    },
+    "bone": {
+        "points": [
+            { "pos": 0.0, "color": [0, 0, 0], "order": 1 },
+            { "pos": 0.375, "color": [84, 84, 116], "order": 1 },
+            { "pos": 0.75, "color": [167, 199, 199], "order": 1 },
             { "pos": 1.0, "color": [255, 255, 255], "order": 1 }
         ]
     }
